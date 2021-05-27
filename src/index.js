9a1a1cfafe96ddbeb2a1570ac6323291b44febfc
Kalib <kalibuas@gmail.com>
2021-05-27 22:26:52 +0800

    lesson 60

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
- class App extends React.Component {
-  render() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null, lon: null, errorMessage: "" };
+
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-      );
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+        this.setState({ lon: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  render() {
+
+   if( this.state.errorMessage && !this.state.lat){
+      return <div>Error : {this.state.errorMessage}</div>
+   }
+   if(!this.state.errorMessage && this.state.lat){
+    return <div>Latitudes: {this.state.lat}</div>
+   }
+   
+   return <div> Loading </div>
+
     
-    return (
-      <div>
-        Latitude: 
-      </div>
-    )
+   
+   
   }
 }
 
-ReactDOM.render(<App />, document.querySelector('#root'))
-
+ReactDOM.render(<App />, document.querySelector("#root"));
